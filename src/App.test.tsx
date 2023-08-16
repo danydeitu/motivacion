ad994313b1f8fe819f613d0f9933ecf6075a565a
Daniel <danydeitu18@gmail.com>
2023-08-16 14:14:41 -0300

    realizo prueba con jest

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,13 @@
+// Archivo: App.test.tsx
+
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './App'; // Importa tu componente App aquí
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App Component', () => {
+  test('should render motivation card elements', () => {
+    render(<App />); // Renderiza el componente App
+
+    // Realiza las aserciones necesarias aquí para verificar que los elementos de la tarjeta se rendericen correctamente
+  });
 });
